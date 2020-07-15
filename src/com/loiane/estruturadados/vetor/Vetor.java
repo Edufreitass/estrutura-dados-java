dbf23378163eff02e827f39c429d2374d3ddee7e
Eduardo Florêncio de Freitas <eduardoflorencio96@gmail.com>
2020-07-15 17:18:41 -0300

    Aula 03 - Adicionar elemento no final do vetor

--- src/com/loiane/estruturadados/vetor/Vetor.java
+++ src/com/loiane/estruturadados/vetor/Vetor.java
@@ -3,10 +3,46 @@ package com.loiane.estruturadados.vetor;
 public class Vetor {
 
 	private String[] elementos;
+	private int tamanho;
 
 	// construtor que vai receber a capacidade de elementos do vetor
 	public Vetor(int capacidade) {
 		this.elementos = new String[capacidade];
+		this.tamanho = 0;
+	}
+	
+	// metodo que adiciona um elemento ao vetor
+	/* public void adiciona(String elemento) {
+		for (int i = 0; i < this.elementos.length; i++) {
+			// verifica se a posição do vetor esta nula
+			if (this.elementos[i] == null) {
+				this.elementos[i] = elemento;
+				break;
+			}
+		}
+	} */
+	
+	/* public void adiciona(String elemento) throws Exception {
+		
+		// verifica se o tamanho é menor que a capacidade do vetor de elementos
+		if (this.tamanho < this.elementos.length) {
+			this.elementos[this.tamanho] = elemento;
+			this.tamanho++;
+		} else {
+			throw new Exception("Vetor já está cheio, não é possível adicionar mais elementos.");
+		}
+		
+	} */
+
+	public boolean adiciona(String elemento) {
+
+		// verifica se o tamanho é menor que a capacidade do vetor de elementos
+		if (this.tamanho < this.elementos.length) {
+			this.elementos[this.tamanho] = elemento;
+			this.tamanho++;
+			return true;
+		}
+		return false;
 	}
 
 }
