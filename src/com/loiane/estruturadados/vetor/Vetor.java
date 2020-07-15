27fd3385c23e4e95368277c6c3837550b9d2900f
Eduardo Florêncio de Freitas <eduardoflorencio96@gmail.com>
2020-07-15 16:56:46 -0300

    Aula 02 - Vetores e Arrays: Introdução

---
+++ src/com/loiane/estruturadados/vetor/Vetor.java
@@ -0,0 +1,12 @@
+package com.loiane.estruturadados.vetor;
+
+public class Vetor {
+
+	private String[] elementos;
+
+	// construtor que vai receber a capacidade de elementos do vetor
+	public Vetor(int capacidade) {
+		this.elementos = new String[capacidade];
+	}
+
+}
