cde55d2dd0dcc95c45eb79b87e6f3ab99d070f5b
Eduardo Freitas <eduardo.florencio@best2bee.com.br>
2020-07-16 11:34:56 -0300

    Correção de Exercícios - Vetores e Arrays: Exer 02 método último Indice

--- src/com/loiane/estruturadados/vetor/Lista.java
+++ src/com/loiane/estruturadados/vetor/Lista.java
@@ -74,6 +74,19 @@ public class Lista<T> {
 		return -1;
 	}
 	
+	// exercicio 02
+	public int ultimoIndice(T elemento) {
+		
+		int ultimaPos = -1;
+		for (int i = this.tamanho - 1; i >=0; i--) {
+			if (this.elementos[i].equals(elemento)) {
+				return i;
+			}
+		}
+		
+		return - 1;
+	}
+	
 	// exercicio 01
 	public boolean contem(T elemento) {
 		
