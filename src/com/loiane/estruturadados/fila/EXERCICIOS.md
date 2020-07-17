8724e91416bb6dab45b9d51626f91f48c0d5f12d
Eduardo Freitas <eduardo.florencio@best2bee.com.br>
2020-07-17 13:10:05 -0300

    Filas: Exer 01 Impressão de Documentos

---
+++ src/com/loiane/estruturadados/fila/EXERCICIOS.md
@@ -0,0 +1,7 @@
+# Estruturas de dados e algoritmos com Java - Exercicios Filas (Queues)
+
+![image](https://user-images.githubusercontent.com/56324728/87806473-54340400-c82d-11ea-966b-595b8ae56efd.png)
+
+![image](https://user-images.githubusercontent.com/56324728/87806494-5ac27b80-c82d-11ea-84fd-3215ca5651bf.png)
+
+[Link do curso](https://www.youtube.com/playlist?list=PLGxZ4Rq3BOBrgumpzz-l8kFMw2DLERdxi)
---
+++ src/com/loiane/estruturadados/fila/exercicios/ex01/Documento.java
@@ -0,0 +1,30 @@
+package com.loiane.estruturadados.fila.exercicios.ex01;
+
+public class Documento {
+
+	private String nome;
+	private int numFolhas;
+
+	public Documento(String nome, int numFolhas) {
+		super();
+		this.nome = nome;
+		this.numFolhas = numFolhas;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getNumFolhas() {
+		return numFolhas;
+	}
+
+	public void setNumFolhas(int numFolhas) {
+		this.numFolhas = numFolhas;
+	}
+
+}
