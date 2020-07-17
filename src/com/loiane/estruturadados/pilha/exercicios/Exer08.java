c4d4b8cf66c35c353fac4796d32daf848aee94d0
Eduardo Freitas <eduardo.florencio@best2bee.com.br>
2020-07-17 11:02:18 -0300

    Correção de Exercícios - Pilhas: Exer 08 Desafio Torre de Hanoi

---
+++ src/com/loiane/estruturadados/pilha/exercicios/Exer08.java
@@ -0,0 +1,37 @@
+package com.loiane.estruturadados.pilha.exercicios;
+
+import java.util.Stack;
+
+public class Exer08 {
+
+	public static void main(String[] args) {
+
+		Stack<Integer> original = new Stack<>();
+		Stack<Integer> destino = new Stack<>();
+		Stack<Integer> auxiliar = new Stack<>();
+
+		original.push(3);
+		original.push(2);
+		original.push(1);
+
+		torreDeHanoi(original.size(), original, destino, auxiliar);
+
+	}
+
+	public static void torreDeHanoi(int qtdDiscos, Stack<Integer> original, Stack<Integer> destino,
+			Stack<Integer> auxiliar) {
+
+		if (qtdDiscos > 0) {
+			torreDeHanoi(qtdDiscos - 1, original, auxiliar, destino);
+			destino.push(original.pop());
+			System.out.println("------");
+			System.out.println("Original: " + original);
+			System.out.println("Destino: " + destino);
+			System.out.println("Auxiliar: " + auxiliar);
+			torreDeHanoi(qtdDiscos - 1, auxiliar, destino, original);
+
+		}
+
+	}
+
+}
